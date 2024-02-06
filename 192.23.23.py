e63ef105665aef61726bd91b929b9e21b47afb02
192.23.23 <130501938+acehere2@users.noreply.github.com>
2024-02-06 13:04:40 +0530

    Add files via upload

--- 192.23.23.py
+++ 192.23.23.py
@@ -15,9 +15,9 @@ else
   apt install figlet toilet python curl -y
   apt install python3-pip
   pip install -r requirements.txt
-  echo This Script Was Made By 192.23.23 > update.speedx
-  echo Requirements Installed....
-  echo Press Enter To Continue...
+  echo "This Script Was Made By 192.23.23" > update.speedx
+  echo "Requirements Installed...."
+  echo "Press Enter To Continue..."
   read upd
 fi
 
@@ -26,9 +26,9 @@ do
   rm *.xxx >/dev/null 2>&1
   clear
   echo -e "\e[1;31m"
-  figlet 192.23.23 Tool
+  figlet "192.23.23 Tool"
   echo -e "\e[1;34m Created By \e[1;32m"
-  toilet -f mono12 -F border 192.23.23
+  toilet -f mono12 -F border "192.23.23"
   echo -e "\e[4;34m This Bomber Was Created By 192.23.23\e[0m"
 
   echo -e "\e[1;34m For Any Queries Mail Me!!!\e[0m"
@@ -45,26 +45,26 @@ do
   echo "Press 5 To Exit"
   read ch
 
-  if [ $ch -eq 1 ];then
+  if [ $ch -eq 1 ]; then
     clear
     echo -e "\e[1;32m"
     rm *.xxx >/dev/null 2>&1
     python3 bomber.py
     rm *.xxx >/dev/null 2>&1
     exit 0
-  elif [ $ch -eq 2 ];then
+  elif [ $ch -eq 2 ]; then
     clear
     echo -e "\e[1;32m"
     echo 'Call Bomb By SpeedX'> call.xxx
     python3 bomber.py call
     rm *.xxx >/dev/null 2>&1
     exit 0
-  elif [ $ch -eq 3 ];then
+  elif [ $ch -eq 3 ]; then
     clear
     apt install git -y
     echo -e "\e[1;34m Downloading Latest Files..."
     git clone https://github.com/TheSpeedX/TBomb
-    if [[ -s TBomb/TBomb.sh ]];then
+    if [[ -s TBomb/TBomb.sh ]]; then
       cd TBomb
       cp -r -f * .. > temp
       cd ..
@@ -79,12 +79,12 @@ do
     read a6
     ./192.23.23.sh
     exit
-  elif [ $ch -eq 4 ];then
+  elif [ $ch -eq 4 ]; then
     clear
     echo -e "\e[1;33m"
-    figlet TBomb
+    figlet "TBomb"
     echo -e "\e[1;34m Created By \e[1;34m"
-    toilet -f mono12 -F border 192.23.23
+    toilet -f mono12 -F border "192.23.23"
     echo  " "
     echo -e "\e[1;32m                   Features\e[1;34m"
     echo "  [+] Unlimited And Super-Fast Bombing"
@@ -110,4 +110,5 @@ do
     echo -e "\e[1;32m Make Sure To Update it If It Does not Work.\e[0m"
     echo  " "
     echo -e "\e[4;31m That's All !!!\e[0m"
-   
+  fi
+done
